0c2c49ca4a485ab4b3e0f3e1aab3a011e2480377
南秋SouthAki <57227318+xieleihan@users.noreply.github.com>
2025-06-18 14:43:46 +0800

    fix:修复action安全机制

--- src/utils/echarts.js
+++ src/utils/echarts.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 async function generateEchartsImage(data) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
     const page = await browser.newPage();
     console.log("✅ Puppeteer 启动成功,传递的数据是", data);
 
